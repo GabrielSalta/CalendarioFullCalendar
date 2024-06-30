68ba5cfb8b1baab5419bb6acb6f7a32314b2445e
GabrielSalta <gabrielsalta@hotmail.com>
2024-06-30 09:44:35 -0300

    Agregar archivos de proyecto.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CalendarioFullCalendar.Controllers
+{
+    public class HomeController : Controller
+    {
+        // Acción para la página principal del calendario
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // Acción para la vista del calendario
+        public IActionResult Calendar()
+        {
+            return View();
+        }
+
+        // Acción para obtener los eventos del calendario en formato JSON
+        [HttpGet]
+        public JsonResult GetCalendarEvents()
+        {
+            var events = new[]
+            {
+                new {
+                    title = "Seminario de actualización en sistemas colaborativos",
+                    start = "2024-03-18",
+                    end = "2024-07-20"
+                }
+            };
+
+            return new JsonResult(events);
+        }
+    }
+}
